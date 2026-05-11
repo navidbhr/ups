931d60fa54eed9fd62bd61f3149a6b20b55f4781
Navid <navidbhr2@gmail.com>
2026-05-11 11:08:20 +0330

    1

--- main/urls.py
+++ main/urls.py
@@ -1,8 +1,7 @@
 from django.urls import path
-from .views import home_view, category_detail_view # اضافه کردن ویو جدید
+from .views import home_view
 
 urlpatterns = [
     path('', home_view, name='home'),
-    # مسیر برای دسته‌بندی‌ها با استفاده از slug
-    path('category/<slug:slug>/', category_detail_view, name='category_detail'),
+
 ]
